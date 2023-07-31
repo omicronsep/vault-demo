60bd0b2104a5fe02212f365967163ec2ca696901
Orlando Ramos <omicron@bhit.com.mx>
2023-07-30 21:45:44 -0600

    vault demo

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -12,7 +12,7 @@ node {
             // engine version can be defined on secret, job, folder or global.
             // the default is engine version 2 unless otherwise specified globally.
             def secrets = [
-                    [path        : 'secrets/creds/my-secret-text', engineVersion: 1,
+                    [path        : 'creds/my-secret-text', engineVersion: 1,
                      secretValues: [
                              [envVar: 'testing', vaultKey: 'secret'],
                              [envVar: 'testing_again', vaultKey: 'clientid']
