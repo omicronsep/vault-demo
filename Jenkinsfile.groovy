981584a180b5d4aac6c127f45189661ec7eb16d4
Orlando Ramos <omicron@bhit.com.mx>
2023-07-30 21:44:03 -0600

    vault demo

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -6,13 +6,13 @@ node {
             // (e.g. folder or global) will be used
             def configuration = [vaultUrl         : 'http://vaultdgtic.sep.gob.mx',
                                  vaultCredentialId: 'vault-jenkins-role',
-                                 engineVersion    : 2]
+                                 engineVersion    : 1]
             // inside this block your credentials will be available as env variables
             // define the secrets and the env variables
             // engine version can be defined on secret, job, folder or global.
             // the default is engine version 2 unless otherwise specified globally.
             def secrets = [
-                    [path        : 'creds/my-secret-text', engineVersion: 1,
+                    [path        : 'secret/creds/my-secret-text', engineVersion: 1,
                      secretValues: [
                              [envVar: 'testing', vaultKey: 'secret'],
                              [envVar: 'testing_again', vaultKey: 'clientid']
